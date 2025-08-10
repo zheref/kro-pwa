e640ad5a2dd30930e9be45650f5bf4ffd05887c3
Sergio Daniel <me@zheref.io>
2025-08-10 15:02:14 -0500

    Discourage semicolons on TS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "semi": "off"
+      "semi": ["error", "never"]
     }
   }
-];
+]
 
-export default eslintConfig;
+export default eslintConfig
